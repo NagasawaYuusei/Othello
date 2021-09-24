ac8c74de52fa5952c6b58a7c632523bfa476db19
NagasawaYusei <curga3013@gmail.com>
2021-09-24 19:04:35 +0900

    シーンを追加

---
+++ Assets/Script/SceneManeger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManeger : MonoBehaviour
+{
+    public void Scene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
